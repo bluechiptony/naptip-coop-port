5a566ee927bf8ded3544885b2e22212172096411
bluechiptony <tony5egwu@gmail.com>
2020-05-16 23:38:33 +0100

    dashboard tab custom

---
+++ src/app/overview/grand-overview/grand-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grand-overview',
+  templateUrl: './grand-overview.component.html',
+  styleUrls: ['./grand-overview.component.scss']
+})
+export class GrandOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
